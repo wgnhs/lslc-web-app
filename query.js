ca48c4dd85fb5321d84d554fbb078abc536a907d
SorenWalljasper <swalljasper@wisc.edu>
2017-02-17 13:37:07 -0600

    concatenate SQL strings and creates live feedback bars
    SQL action still commented out

--- query.js
+++ query.js
@@ -1,6 +1,7 @@
 //initial comment for filter branch
 var rockSearch = false
 var countySearch = false
+var rockTypeSearchBar = document.getElementById('rockTypeSearch') //gets search bar element
 
 function queryCallback(queryResult){
    //console.log(queryResult);
@@ -81,12 +82,14 @@ function utilizeButtons(selectionTool, Draw, fl){
 //function utilizes the search bars; called from map.js
 function initSearchBars(Query, QueryTask){
 
+		//creates two booleans -- checks to see whether the boxes are checked or not 
+	var thinSectionChecked = document.getElementById("thinSectionCheckbox").checked
+	var handSampleChecked = document.getElementById("handSampleCheckbox").checked
+
 	//declares empty strings for search keys
 	var rockTypeSearchKey = "" 
 	var countySearchKey = ""
 
-	//
-	var rockTypeSearchBar = document.getElementById('rockTypeSearch') //gets search bar element
 	//function called everytime key is lifted in searchbar
 	rockTypeSearchBar.onkeyup = function(){
 		rockSearch = true //indicates that there is a rock filter applicable
@@ -96,7 +99,7 @@ function initSearchBars(Query, QueryTask){
     	$("#rockSearchOn").remove();
     	$("#filterFeedback").append($("<div id='rockSearchOn' class='feedbackBar'></div>"))
     	$("#rockSearchOn").append($("<p>" + rockTypeSearchKey + "</p>"))
-    	queryTableForFilters(rockTypeSearchKey, countySearchKey, Query, QueryTask)
+    	queryTableForFilters(rockTypeSearchKey, countySearchKey, Query, QueryTask, thinSectionChecked, handSampleChecked)
 	}
 
 	var countySearchBar = document.getElementById('countySearch') //gets search bar element
@@ -109,7 +112,19 @@ function initSearchBars(Query, QueryTask){
     	$("#countySearchOn").remove();
     	$("#filterFeedback").append($("<div id='countySearchOn' class='feedbackBar'></div>"))
     	$("#countySearchOn").append($("<p>" + countySearchKey + "</p>"))
-    	queryTableForFilters(rockTypeSearchKey, countySearchKey, Query, QueryTask)
+    	queryTableForFilters(rockTypeSearchKey, countySearchKey, Query, QueryTask, thinSectionChecked, handSampleChecked)
+	}
+
+	thinSectionCheckbox.onchange = function() {
+
+		if (thinSectionChecked) { thinSectionChecked = false } else { thinSectionChecked = true }
+		queryTableForFilters(rockTypeSearchKey, countySearchKey, Query, QueryTask, thinSectionChecked, handSampleChecked)
+	}
+
+	handSampleCheckbox.onchange = function() {
+
+		if (handSampleChecked) { handSampleChecked = false } else { handSampleChecked = true }
+		queryTableForFilters(rockTypeSearchKey, countySearchKey, Query, QueryTask, thinSectionChecked, handSampleChecked)
 	}
 
 	
@@ -118,11 +133,18 @@ function initSearchBars(Query, QueryTask){
 }
 
 
-function queryTableForFilters(rockTypeSearchKey, countySearchKey, Query, QueryTask){
+function queryTableForFilters(rockTypeSearchKey, countySearchKey, Query, QueryTask, thinSectionChecked, handSampleChecked){
 
-	//creates two booleans -- checks to see whether the boxes are checked or not 
-	var thinSectionChecked = document.getElementById("thinSectionCheckbox").checked
-	var handSampleChecked = document.getElementById("handSampleCheckbox").checked
+	//checks each possible SQL statement, creates SQL statement if applicable, or defines a blank string
+	if (rockSearch) { var rockTypeString = "RockType LIKE '%"+rockTypeSearchKey+"%'"} else { var rockTypeString = ""}
+   	if (countySearch) { var countySearchString = "County LIKE '%"+countySearchKey+"%'"} else { var countySearchString = ""}
+   	if (thinSectionChecked) { var thinSectionString = "ThinsectionCount > 0"} else { var thinSectionString = ""}
+   	if (handSampleChecked) { var handSampleString = "HandSampleCount > 0"} else { var handSampleString = ""}
+
+   	//combines search strings into an array
+   	var sqlArray = [rockTypeString, countySearchString, thinSectionString, handSampleString]
+
+	
     
     //same lines from QueryTable() 
    	var query = new Query(); 
@@ -130,53 +152,66 @@ function queryTableForFilters(rockTypeSearchKey, countySearchKey, Query, QueryTa
    	query.returnGeometry = false;
    	query.where = "" // assigns empty string to where statement
 
-   	//checks to see whether or not rock type has a filter; applies it if so
-   	if (rockSearch){	
-   		query.where = "RockType LIKE %"+rockTypeSearchKey+"%";
+   	
+   	//iterates through sqlArray
+   	for (i = 0; i < sqlArray.length; i++) {
+   		//testing for a value before and after, if so, adds AND inbtween them
+   		if (query.where != "" && sqlArray[i] != "") {query.where +=  " AND " }
+   		query.where += sqlArray[i] //adds the sql querry to the string 
    	}
 
-   	//checks to see whether or not county has a filter; applies it if so
-   	if (countySearch){
-   		//if there's an existing filter applied, adds AND to combine
-   		if (query.where != ""){
-   			query.where = query.where + " AND "
-   		}
-   		query.where = query.where + "County LIKE %"+countySearchKey+"%";
-   	}
+   	
 
-   	//checks to see whether or not thin section box is checked; applies it if so
-   	if (thinSectionChecked){
-   		//if there's an existing filter applied, adds AND to combine
-   		if (query.where != ""){
-   			query.where = query.where + " AND "
-   		}
-   		query.where = query.where + "ThinsectionCount > 0";
-   	}
+   	console.log(query.where)
 
-   	//checks to see whether or not thin section box is checked; applies it if so
-   	if (handSampleChecked){
-   		//if there's an existing filter applied, adds AND to combine
-   		if (query.where != ""){
-   			query.where = query.where + " AND "
-   		}
-   		query.where = query.where + "HandSampleCount > 0";
-   	}
+   	
 
 
-   	console.log(query.where) 
       
    //url to samples table
-   	// var queryTask = new QueryTask("http://geodata.wgnhs.uwex.edu/arcgis/rest/services/lslc/lslc/MapServer/1");
+  //  	var queryTask = new QueryTask("http://geodata.wgnhs.uwex.edu/arcgis/rest/services/lslc/lslc/MapServer/1");
    
-   	// queryTask.execute(
-    //                  query, 
-    //                   function(queryResult){
+  //  	queryTask.execute(
+  //                    query, 
+  //                     function(queryResult){
                          
-    //                       queryCallback(queryResult);}
+  //                         queryCallback(queryResult);}
                     
-    //                  );
+  //                    );
    
   // queryTask.executeForIds(query, queryCallback);
+
+  //calls remove filters
+   	removeFilters(rockTypeSearchKey, countySearchKey, Query, QueryTask, thinSectionChecked, handSampleChecked)
+}
+
+function removeFilters(rockTypeSearchKey, countySearchKey, Query, QueryTask, thinSectionChecked, handSampleChecked){
+	
+	//onclick function called when someone clicks on a feedback div
+	$("#rockSearchOn").click(function() {
+
+   		//removes feedback div; sets the search key to remove statement from SQL Query
+   		$("#rockSearchOn").remove();
+   		rockSearch = false 
+    	rockTypeSearchKey = "" 
+
+    	//resets the SQL Query
+    	queryTableForFilters(rockTypeSearchKey, countySearchKey, Query, QueryTask, thinSectionChecked, handSampleChecked)
+
+   	})
+
+	//onclick function called when someone clicks on a feedback div
+   	$("#countySearchOn").click(function() {
+   		
+   		//removes feedback div; sets the search key to remove statement from SQL Query
+   		$("#countySearchOn").remove();
+   		countySearch = false 
+    	countySearchKey = "" 
+
+    	//resets the SQL Query
+    	queryTableForFilters(rockTypeSearchKey, countySearchKey, Query, QueryTask, thinSectionChecked, handSampleChecked)
+
+   	})
 }
 
 
