6e2d372add5e40e9b61edef1d2344fd9314f6b03
cmrRose <cmr.rose@gmail.com>
2017-02-20 09:53:13 -0600

    Move utilizeButtons and initMapButtons functions to map.js

--- query.js
+++ query.js
@@ -33,51 +33,6 @@ function queryTable(selectedSections, Query, QueryTask){
 }
 
 
-function initMapButtons(event, selectionTool, Draw, Query, on, fl){
-    
-   //selectionTool is a global variable
-    selectionTool = new Draw(event.map); 
-    var mapSelection = new Query();
-
-   //when the selection tool has finished drawing a box, 
-   // create a new selection in the feature layer (fl) 
-    on(selectionTool, "DrawEnd", function(geometry){
-    	console.log('ran')
-      selectionTool.deactivate();
-       mapSelection.geometry = geometry;
-       fl.selectFeatures(mapSelection, fl.SELECTION_NEW);
-
-       
-    });
-
-  utilizeButtons(selectionTool, Draw, fl); //calls funtion that has jquery onclick functions
-} 
-
-/********** SELECTION/CLEAR FUNCTIONALITY **********/
-function utilizeButtons(selectionTool, Draw, fl){
-
-   $("#mapSelectionButton").on( "click", function(){
-
-	
-       console.log(selectionTool)
-       selectionTool.activate(Draw.EXTENT);
-       
-   });
-   
-   $("#mapClearButton").on("click", function () {
-       //console.log("clear map selection");
-        fl.clearSelection();
-       
-       
-       //FAKE LINK!! 
-       //CLEAR THE RESULTS LIST! 
-      console.log("reset list"); 
-        $("#resultsUL").html('');
-        $("#resultCount").html('0');
-            
-    });
-
-}
 
 //function utilizes the search bars; called from map.js
 function initSearchBars(Query, QueryTask){
@@ -213,20 +168,3 @@ function removeFilters(rockTypeSearchKey, countySearchKey, Query, QueryTask, thi
 
    	})
 }
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
