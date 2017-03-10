8927f7d2db6fc8c4be8c4c943935d2297514fab2
cmrRose <cmr.rose@gmail.com>
2017-03-10 11:58:19 -0600

    add a header to the sample record page

---
+++ header.js
@@ -0,0 +1,4 @@
+var header = "<div><img src='images/LSLCLogo_v1_lightLake.png' alt='LSLC Logo'/></div><nav><a href='ma'>Home</a><a href='mapView_esri3-18.html'>Filter Samples</a><a href='sampleRecord.html'>Sample Search</a></nav>";
+
+
+$("#header").append(header);
