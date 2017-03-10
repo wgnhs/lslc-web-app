36982534abce413065c5149fcc89a97202e088b7
cmrRose <cmr.rose@gmail.com>
2017-03-10 14:42:05 -0600

    header edits

--- header.js
+++ header.js
@@ -1,4 +1,4 @@
-var header = "<div><img src='images/LSLCLogo_v1_lightLake.png' alt='LSLC Logo'/></div><nav><a href='ma'>Home</a><a href='mapView_esri3-18.html'>Filter Samples</a><a href='sampleRecord.html'>Sample Search</a></nav>";
+var header = "<img src='images/LSLCLogo_v3_lightLake.png' alt='The Lake Superior Legacy Collection Logo'/><nav><a href=''>Home</a><a href='mapView_esri3-18.html'>Filter Samples</a><a href='sampleRecord.html#15'>Sample Search</a><a href=''>About</a></nav>";
 
 
 $("#header").append(header);
