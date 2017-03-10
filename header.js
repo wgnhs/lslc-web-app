7c046e0ac20cef879bbe6172188d1955b2f5cc41
cmrRose <cmr.rose@gmail.com>
2017-03-10 15:03:58 -0600

    change header to links in top right

--- header.js
+++ header.js
@@ -1,4 +1,4 @@
-var header = "<img src='images/LSLCLogo_v3_lightLake.png' alt='The Lake Superior Legacy Collection Logo'/><nav><a href=''>Home</a><a href='mapView_esri3-18.html'>Filter Samples</a><a href='sampleRecord.html#15'>Sample Search</a><a href=''>About</a></nav>";
+var header = "<img src='images/LSLCLogo_v3_lightLake.png' alt='The Lake Superior Legacy Collection Logo'/><!--<span>geologic samples from the Lake Superior Division of the USGS, 1882-1922</span>--><nav><a href=''>Home</a><a href='mapView_esri3-18.html'>Filter Samples</a><a href='sampleRecord.html#15'>Sample Search</a><a href=''>About</a></nav>";
 
 
 $("#header").append(header);
