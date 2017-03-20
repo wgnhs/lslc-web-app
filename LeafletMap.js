552c4cc908bc818987189e371a4886249ea9ce3f
cmrRose <cmr.rose@gmail.com>
2017-03-20 17:34:34 -0500

    add leaflet map and L.esri query.
    Still need to implement: clearing a map filter, highlighting the map
    based on choropleth classes.

--- LeafletMap.js
+++ LeafletMap.js
@@ -4,7 +4,7 @@ var leafletMap = (function(){
     var leafletFeatureLayer;
     
     var initialize = function(){
-        console.log("Leaflet map setup."); 
+        //console.log("Leaflet map setup."); 
         
        
         
@@ -30,60 +30,176 @@ var leafletMap = (function(){
     
     function setupMapButtons(map){
         //called by the map's on load event. 
-        console.log("set up leaflet map buttons.");
+        //console.log("set up leaflet map buttons.");
         
         // FeatureGroup is to store editable layers
         var drawnItems = new L.FeatureGroup;
-        var drawnLayer;
+        
         map.addLayer(drawnItems);
-        
- 
-         var drawControl = new L.Control.Draw({
-             edit: false,
-           //  {
-            //     featureGroup: drawnItems
-                 //,
-                 //poly: {allowIntersection: false}
-           //  },
+
+        var drawControl = new L.Control.Draw({
+             edit: 
+                 false,
+//             {
+//                 featureGroup: drawnItems
+//                 ,
+//                 poly: {allowIntersection: false}
+//             },
              draw: {
-                 marker: false, polyline: false, polygon: false, circle: false, rectangle: {repeatMode: false}
-                 //,
-                // polygon: {allowIntersection: false}
+                 marker: false, polyline: false, circle: false, rectangle: {repeatMode: false}
+                    //not sure why, but circle doesn't seem to work with the query. 
+                 ,
+                 polygon: {allowIntersection: false}
              }
-             
-         });
-         map.addControl(drawControl);
-        
-        map.on (L.Draw.Event.DRAWSTART, function(){
-            console.log("clear all ", drawnLayer);
-            
-           // drawnItems.clearLayers(); //seems to do nothing
-           //map.removeLayer(drawnLayer); //nothing 
-            //map.removeLayer(drawnItems); //nothing
-            //L.EditToolbar.Delete.removeAllLayers(); //not a function
-            
+
         });
+        map.addControl(drawControl);
         
+        //use this if you want something to happen on draw start. 
+        //  map.on (L.Draw.Event.DRAWSTART, function(){
+        //      console.log("draw start ");
+          
+        //  });
+    
         map.on(L.Draw.Event.CREATED, function (e) {
-            console.log("draw stop");
 
-           var layer = e.layer;
+           
+            //To only allow one selection, clear the layer when a new shape is completed. 
+            if (drawnItems && drawnItems.getLayers().length !== 0){
+                drawnItems.clearLayers();
+            }
+            
+            var layer = e.layer;
+            drawnItems.addLayer(layer);
 
-          drawnLayer = map.addLayer(layer);
+            queryGeom(layer);
             
             
         });
         
+        
+//        layer.on('click', function (e){
+//            
+//        })
        
     }
+    function queryGeom(layer){
+        //takes in a rectangle from a draw event 
+        var inputGeom = layer;
+        
+        console.log ("input", inputGeom);
+        
+//        leafletFeatureLayer.query()[intersect](inputGeom).ids(function(error, ids){
+//            console.log(ids);
+//        })
+        
+        var query = L.esri.query({url:"http://geodata.wgnhs.uwex.edu/arcgis/rest/services/lslc/lslc/MapServer/0"}); 
+        query.intersects(inputGeom);
+        
+        query.run(function(error, featureCollection, response){
+            console.log('Found ' + featureCollection.features.length + ' sections');
+           // console.log("features", featureCollection.features);
+            
+            var selectedSections= [];
+            for (i in featureCollection.features){
+                var selectedSectionId =featureCollection.features[i].properties.UID;
+                selectedSections.push(selectedSectionId);
+            }
+           // console.log("filter based on these sections:", selectedSections);
+            filterForSections(selectedSections);
+        });
+    }
     
-    function updateMapFilter(){
-        console.log("update map filter");
+    function filterForSections(array){
+        //console.log("update map filter/filter for sections");
+        
+        filters.mapSectionsInput = array; 
+        queryTableForFilters();
+        
     }
     
     function calculateClasses(array){
         console.log("calculate classes.");
+         console.log("highlight the map sections", array);
+    //set the symbol to the variable highlightSymbol (an object defined above)
+
+    //filters out redundant section ids 
+    var sortedArray = array.sort(function(a,b){return a-b});
+    var filteredSections = []
+
+    for (i = 0; i < sortedArray.length; i++){
+      if (sortedArray[i] != sortedArray[i-1]) {
+        filteredSections.push(sortedArray[i])
+      }
     }
+       
+    //CHOROPLETH-----------------------------------------
+        var choroplethStructureArray = [];
+        //populates 2D array with [[sectionID, # of records in it],,]
+        for (i = 0; i < sortedArray.length; i++){
+          if (sortedArray[i] != sortedArray[i-1]){
+            choroplethStructureArray.push([sortedArray[i], 1]);
+          } else {
+            choroplethStructureArray[choroplethStructureArray.length-1][1]++;
+          }
+        }
+
+        //creates array with just the # of records; later used to find class breaks
+        var classBreaksFinderArray = [];
+        for (i = 0; i < choroplethStructureArray.length; i++){
+          classBreaksFinderArray.push(choroplethStructureArray[i][1]);
+        }
+
+        //sorts the array containing # of samples; sorts and filteres ot redundant values
+        //gives us unique count numbers
+        var sortedClassFinderArray = classBreaksFinderArray.sort(function(a,b){return a-b});
+        var filteredValuesArray = [];
+        for (i = 0; i < sortedClassFinderArray.length; i++){
+          if (sortedClassFinderArray[i] != sortedClassFinderArray[i-1]) {
+            filteredValuesArray.push(sortedClassFinderArray[i])
+          }
+        }
+
+        //sets up 4 class break values using jerry-rigged equal interval -- maybe switch and import D3?
+        var break0 = filteredValuesArray[0];
+        var break1 = filteredValuesArray[Math.round((filteredValuesArray.length / 4) - 1)];
+        var break2 = filteredValuesArray[Math.round((filteredValuesArray.length / 2) - 1)];
+        var break3 = filteredValuesArray[Math.round((filteredValuesArray.length / (4/3)) - 1)];
+        var breakTop = filteredValuesArray[filteredValuesArray.length - 1];
+
+        //defines arrays to be populated with section ids in each class
+        var class1Array = [];
+        var class2Array = [];
+        var class3Array = [];
+        var class4Array = [];
+
+        //populates each class array by checking how many samples they each have, evaluating via breaks
+        //makes use of the 2D choroplethStructureArray
+        for (i = 0; i < choroplethStructureArray.length; i++){
+          if (choroplethStructureArray[i][1] <= break1){
+            class1Array.push(choroplethStructureArray[i][0]);
+          } else if (choroplethStructureArray[i][1] <= break2) {
+            class2Array.push(choroplethStructureArray[i][0]);
+          } else if (choroplethStructureArray[i][1] <= break3) {
+            class3Array.push(choroplethStructureArray[i][0]);
+          } else {
+            class4Array.push(choroplethStructureArray[i][0]);
+          }; 
+        } 
+
+        //tests out how the choropleth system is working
+        console.log("filtered values array -->", filteredValuesArray)
+        console.log("class breaks -->", break0,break1,break2,break3,breakTop)
+       
+
+        //create new Feature Layer with each of these
+        
+        
+        return {"class1Array": class1Array, "class2Array": class2Array, "class3Array": class3Array, "class4Array": class4Array};
+        
+        
+        
+    } //end calculateClasses
     
     function styleSections (){
         console.log ("style sections");
@@ -91,11 +207,25 @@ var leafletMap = (function(){
     
     var highlight = function (array){
         console.log("highlight via Leaflet");
-        calculateClasses(array); 
+        var classes = calculateClasses(array); 
+        
+        console.log("class 1 array:", classes.class1Array);
+        console.log("class 2 array:", classes.class2Array);
+        console.log("class 3 array:", classes.class3Array);
+        console.log("class 4 array:", classes.class4Array);
+        
+          /*
+        var mapHighlight = new Query();
+        mapHighlight.where = ('UID IN ('+filteredSections+')');
+
+        fl.selectFeatures(mapHighlight, fl.SELECTION_NEW); 
+        //zoom to the extent of the filter results.
+*/
     }
     
     return {
         "initialize": initialize,
+        
         "highlight": highlight
     }
     
---
+++ LeafletQuery.js
@@ -0,0 +1,184 @@
+//global var for all filters
+
+var filters = {
+    //all initial values in this filters object should be falsy. 
+    //an empty string is falsy. 
+    //an empty array is not falsy, so the map sections input must be set to null when cleared. 
+    
+    "mapSectionsInput": null, "rockTypeInput": "", "countyInput": "", "handSampleAvailabilityInput": null, "thinSectionAvailabilityInput": null, "stateInput": null 
+};
+
+$(window).on("load", function(){
+    
+    //This sets an event listener for inputs. 
+    initSearchBars();
+    //initialize a listener for filter removal through a click on a filter indicator
+    removeFilters();
+    
+    //call whenever the filters object has changed (EXCEPT map filter). 
+    resetFilters();   
+
+});
+
+
+function initSearchBars(){
+    //called once on load
+    //event listener for a filter input 
+    
+    $("#filters").on("input", "input", function(){
+        resetFilters();
+
+    }); //close #filters.on input function
+ 
+} //end initSearchBars function
+
+function removeFilters(){
+    //called once on load
+	//when the user clicks on a filter indicator... 
+    $("#filterFeedback").on("click", "span", function(){
+      
+        console.log("clear", filters[this.getAttribute('data')]);
+        //reset the filters variable. 
+        filters[this.getAttribute('data')] = null;
+        
+        this.remove();
+        //reset the value in the input / searchbar
+        if (this.getAttribute('data') == 'rockTypeInput'){console.log("clear rock type."); $("#rockTypeSearch").val('');};
+        if (this.getAttribute('data') == 'countyInput'){console.log("clear county"); $("#countySearch").val('');};
+        if (this.getAttribute('data') == 'stateInput'){console.log("clear state"); $("#stateSearch").val('');};
+        if (this.getAttribute('data') == 'handSampleAvailabilityInput'){console.log("clear handsample"); document.getElementById("handSampleCheckbox").checked = false;};
+        if (this.getAttribute('data') == 'thinSectionAvailabilityInput'){console.log("clear thin section"); document.getElementById("thinSectionCheckbox").checked = false;};
+        
+        //resetFilters will call QueryTable. 
+        resetFilters();
+    });
+    
+  
+}
+    
+    
+function resetFilters() {
+        filters.rockTypeInput = $("#rockTypeSearch").val();
+        filters.countyInput = $("#countySearch").val(); 
+        filters.stateInput = $("#stateSearch").val();
+        filters.thinSectionAvailabilityInput =  document.getElementById("thinSectionCheckbox").checked;
+        filters.handSampleAvailabilityInput = document.getElementById("handSampleCheckbox").checked;
+        
+       // console.log("filters set:", filters);
+        queryTableForFilters();
+}
+    
+    
+function queryTableForFilters(){
+   
+    var newsqlArray = ["1=1"];
+    
+    //delete all filter indicators. They will be replaced. 
+    $("#filterFeedback").html('');
+    
+    if (filters.rockTypeInput) {
+        newsqlArray.push("Upper(RockType) LIKE Upper('%"+filters.rockTypeInput+"%')");
+        //adds feedback indicator
+    	//$("#rockSearchOn").remove();
+    	$("#filterFeedback").append($("<span id='rockSearchOn' class='feedbackBar' data='rockTypeInput'>rock:&nbsp" + filters.rockTypeInput + "<img src='images/close.png' style = 'height: 21px; margin-bottom: -5px;'/></span>"));
+        }; 
+    if (filters.countyInput) {
+        newsqlArray.push("Upper(County) LIKE Upper('%"+filters.countyInput+"%')");
+       // $("#countySearchOn").remove();
+    	$("#filterFeedback").append($("<span id='countySearchOn' class='feedbackBar' data = 'countyInput'>county:&nbsp" + filters.countyInput +"<img src='images/close.png' style = 'height: 21px; margin-bottom: -5px;'/></span>"));
+        }; 
+    if (filters.handSampleAvailabilityInput) {
+        newsqlArray.push("HandSampleCount > 0");
+       // $("#handSampleOn").remove();
+    	$("#filterFeedback").append($("<span id='handSampleOn' class='feedbackBar' data='handSampleAvailabilityInput'>Hand&nbspsample:&nbsp" + filters.handSampleAvailabilityInput + "<img src='images/close.png' style = 'height: 21px; margin-bottom: -5px;'/></span>"));
+        }; 
+    if (filters.thinSectionAvailabilityInput) {
+        newsqlArray.push("ThinsectionCount > 0");
+        $("#filterFeedback").append($("<span id='thinSectionOn' class='feedbackBar' data='thinSectionAvailabilityInput'>Thin&nbspsection:&nbsp" + filters.thinSectionAvailabilityInput + "<img src='images/close.png' style = 'height: 21px; margin-bottom: -5px;'/></span>"));
+        }; 
+    if (filters.mapSectionsInput) {newsqlArray.push("SectionId IN ("+filters.mapSectionsInput+")");}; 
+    if (filters.stateInput){
+        newsqlArray.push("Upper(State) LIKE Upper('%"+filters.stateInput+"%')");
+        $("#filterFeedback").append($("<span id='stateOn' class='feedbackBar' data='stateInput'>state:&nbsp" + filters.stateInput + "<img src='images/close.png' style = 'height: 21px; margin-bottom: -5px;'/></span>"));
+        };
+    
+    //console.log ("new SQL array:", newsqlArray);
+    
+   
+    var whereString = "";
+
+   	//iterates through newsqlArray
+    //THIS IS UNNECESSARY if we always search for everything when no filters are applied. 
+    for (i = 0; i < newsqlArray.length; i++) {
+   		//testing for a value before and after, if so, adds AND in between them
+   		if (whereString != "" && newsqlArray[i] != "") {whereString +=  " AND " };
+   		whereString += newsqlArray[i]; //adds the sql query to the string 
+        
+   	};
+    
+     var samplesQuery = L.esri.query({url:"http://geodata.wgnhs.uwex.edu/arcgis/rest/services/lslc/lslc/MapServer/1"}); //url to samples table
+    samplesQuery.fields(["*"]);
+    samplesQuery.returnGeometry(false);
+   // samplesQuery.where(""); // assigns empty string to where statement
+    samplesQuery.where(whereString);
+    
+    console.log("query where is:", whereString);
+     //set the sections query where clause to the same where as the normal query. 
+   // sectionsQuery.where = samplesQuery.where;
+   
+   //only try to send queryTask if there is something to search on.
+    if (samplesQuery.where.length > 0){
+        
+        if (samplesQuery.where === "1=1"){ 
+            console.log("Narrow the results by applying filters above.");
+        } else {
+            
+            console.log("run the query.");
+            
+            samplesQuery.run(function(error, result, response){
+                console.log('result', result);
+                console.log("response", response.features);
+                
+                listResults(response);
+                
+                //iterate through and output array of sections ofr the highlight function. 
+                var highlightMapSections = []; 
+                //iterate through  
+                for (f in response.features){
+                   // console.log("f attributes sectionId", response.features[f].attributes.SectionId);
+                    highlightMapSections.push(response.features[f].attributes.SectionId);
+                }
+                leafletMap.highlight(highlightMapSections);
+                
+            });
+        
+            
+        }
+        
+   	           
+   
+    
+ /*    queryTask.execute( query, function(samplesResult){listResults(samplesResult);}  );
+    queryTask.execute( sectionsQuery, function(sectionsQueryResult){
+       // console.log("sections query result:", sectionqueryResult.features);
+        var highlightMapSections = []; 
+        //iterate through  
+        for (f in sectionsQueryResult.features){
+           // console.log("f attributes sectionId", sectionqueryResult.features[f].attributes.SectionId);
+            highlightMapSections.push(sectionsQueryResult.features[f].attributes.SectionId);
+        }
+        
+        highlightMap(highlightMapSections, fl);
+    });
+
+ 
+*/
+    } else {
+        //if query.where.length is 0 or less, query for everything! 
+        console.log("query is empty.");
+        
+    }
+     
+    
+    
+} //end queryTableForFilters
