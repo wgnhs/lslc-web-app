3e322b72ef3a77239adb8702b78dd181df4f8b91
cmrRose <cmr.rose@gmail.com>
2017-03-20 12:11:33 -0500

    fix link

--- header.js
+++ header.js
@@ -1,4 +1,4 @@
-var header = "<img src='images/LSLCLogo_v3_lightLake.png' alt='The Lake Superior Legacy Collection Logo'/><!--<span>geologic samples from the Lake Superior Division of the USGS, 1882-1922</span>--><nav><a id='navHome' href=''>Home</a><a id='navFilter'href='mapView_esri3-18.html'>Filter Samples</a><a id='navSample' href='sampleRecord.html'>Sample Search</a><a id='navAbout' href=''>About</a></nav>";
+var header = "<img src='images/LSLCLogo_v3_lightLake.png' alt='The Lake Superior Legacy Collection Logo'/><!--<span>geologic samples from the Lake Superior Division of the USGS, 1882-1922</span>--><nav><a id='navHome' href=''>Home</a><a id='navFilter'href='filterSamples.html'>Filter Samples</a><a id='navSample' href='sampleRecord.html'>Sample Search</a><a id='navAbout' href=''>About</a></nav>";
 
 
 $("#header").append(header);
