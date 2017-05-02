a6f4613330ea3f163930621ca7d9a32419cbd2e2
cmrRose <cmr.rose@gmail.com>
2017-05-02 17:26:13 -0500

    creat drop-downs in the nav bar. Begin a notebooks list page.

--- header.js
+++ header.js
@@ -1,4 +1,8 @@
-var header = "<a href='index.html'><img src='images/LSLCLogo_v3_lightLake.png' alt='The Lake Superior Legacy Collection Logo'/></a><!--<span>geologic samples from the Lake Superior Division of the USGS, 1882-1922</span>--><nav><a id='navHome' href='index.html'>Home</a><a id='navFilter'href='filterSamples.html'>Filter Samples</a><a id='navSample' href='sampleRecord.html'>Sample Details</a><a id='navAbout' href=''>About</a><a id='navContact' href='contact.html'>Contact</a></nav>";
+//add html content for the header (nhome link, nav bar) and the footer. 
+//each html page must have a header and a footer div. 
+
+var header = "<a href='index.html'><img src='images/LSLCLogo_v3_lightLake.png' alt='The Lake Superior Legacy Collection Logo'/></a><!--<span>geologic samples from the Lake Superior Division of the USGS, 1882-1922</span>-->";
+header += "<nav><a id='navHome' href='index.html' class='navMainLink'>Home</a><div id='navCollection' href='#' class='navMainLink dropdownContainer'>Collection<div class='dropdown-content'><a id='navFilter'href='filterSamples.html'>Filter Samples</a><a id='navSample' href='sampleRecord.html'>Sample Details</a><a href='notebooks.html'>List of Notebooks</a></div></div><div id='navAbout' href='#' class='navMainLink dropdownContainer'>About<div class='dropdown-content'><a href='#'>Lake Superior Division, 1882-1922</a><a href=''>Digitizing the collection</a></div></div><a id='navContact' href='contact.html' class='navMainLink'>Contact</a></nav>";
 
 
 $("#header").append(header);
