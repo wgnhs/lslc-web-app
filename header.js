67ba22c4a8b964f9a6309bc6c1c81e7e6fe4ac25
cmrRose <cmr.rose@gmail.com>
2017-05-25 15:19:13 -0500

    change name of image file.

--- header.js
+++ header.js
@@ -1,7 +1,7 @@
 //add html content for the header (nhome link, nav bar) and the footer. 
 //each html page must have a header and a footer div. 
 
-var header = "<a href='index.html'><img src='images/LSLCLogo_v3_lightLake.png' alt='The Lake Superior Legacy Collection Logo'/></a><!--<span>geologic samples from the Lake Superior Division of the USGS, 1882-1922</span>-->";
+var header = "<a href='index.html'><img id='homeLogo' src='images/lslc-logo_v4_light-lake_smaller.png' alt='The Lake Superior Legacy Collection Logo'/></a><!--<span>geologic samples from the Lake Superior Division of the USGS, 1882-1922</span>-->";
 
 header += "<div class='striped-1 messageBar'><span>This webpage is currently under construction. You may encounter broken hyperlinks or other errors.</span></div>";
 
