b1fe2127463f8f1f624e83355c0a17d0116435a3
David M Sibley <sibley@wisc.edu>
2022-03-08 13:31:55 -0600

    Update mapServiceUrls.js

--- mapServiceUrls.js
+++ mapServiceUrls.js
@@ -1,11 +1,11 @@
 //vars for all map service URLs: 
-var PLSSSectionsLayerURL = "https://geodata.wgnhs.uwex.edu/arcgis/rest/services/lslc/MapServer/0"; 
-var handSampleTableURL = "https://geodata.wgnhs.uwex.edu/arcgis/rest/services/lslc/MapServer/1";
-var notebooksTableURL = "https://geodata.wgnhs.uwex.edu/arcgis/rest/services/lslc/MapServer/2";
-var thinSectionsTableURL = "https://geodata.wgnhs.uwex.edu/arcgis/rest/services/lslc/MapServer/3"; 
-var lithologyTableURL = "https://geodata.wgnhs.uwex.edu/arcgis/rest/services/lslc/MapServer/4";
-var ledgerTableURL = "https://geodata.wgnhs.uwex.edu/arcgis/rest/services/lslc/MapServer/5";
-var samplesTableURL = "https://geodata.wgnhs.uwex.edu/arcgis/rest/services/lslc/MapServer/6"; 
+var PLSSSectionsLayerURL = "https://data.wgnhs.wisc.edu/arcgis/rest/services/lslc/MapServer/0"; 
+var handSampleTableURL = "https://data.wgnhs.wisc.edu/arcgis/rest/services/lslc/MapServer/1";
+var notebooksTableURL = "https://data.wgnhs.wisc.edu/arcgis/rest/services/lslc/MapServer/2";
+var thinSectionsTableURL = "https://data.wgnhs.wisc.edu/arcgis/rest/services/lslc/MapServer/3"; 
+var lithologyTableURL = "https://data.wgnhs.wisc.edu/arcgis/rest/services/lslc/MapServer/4";
+var ledgerTableURL = "https://data.wgnhs.wisc.edu/arcgis/rest/services/lslc/MapServer/5";
+var samplesTableURL = "https://data.wgnhs.wisc.edu/arcgis/rest/services/lslc/MapServer/6"; 
 
 //vars for table field names 
 var PlssField = 'PlssId'; 
